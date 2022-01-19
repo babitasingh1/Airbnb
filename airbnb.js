6913a148e7f380066269a8936439f75a7145cd20
COOL\babita.singh <babita.singh1@gmail.com>
2022-01-19 14:47:23 +0100

    change heading colors on scroll

--- airbnb.js
+++ airbnb.js
@@ -2,19 +2,32 @@ const subHeadrDivs = document.querySelectorAll(".subHeaderDivs");
 const mainheader = document.querySelector(".mainHeader");
 const subHeader = document.querySelector(".subHeader");
 const scrollHeader = document.querySelector(".headerOnScroll");
+const globeIcon = document.querySelector(".fa-globe");
+const siteName = document.querySelector(".mainHeader-divisions-1__name");
+const siteIcon = document.querySelector(".fa-airbnb");
+const rightCornerHeadings = document.querySelector(
+  ".mainHeader-divisions-3__headings"
+);
 console.log(subHeadrDivs);
 function hidescr() {
   if (window.scrollY > 0) {
-    subHeader.classList.add("subHeader_hide");
-    mainheader.classList.add("mainHeader_hide");
+    // subHeader.classList.add("subHeader_hide");
+    // mainheader.classList.add("mainHeader_hide");
+    rightCornerHeadings.classList.add("changeColorToBlack");
+    globeIcon.classList.add("changeColorToBlack");
+    siteName.classList.add("changeColorToPink");
+    siteIcon.classList.add("changeColorToPink");
 
-    scrollHeader.classList.add("headerOnScroll_show");
-    console.log("hide");
+    //scrollHeader.classList.add("headerOnScroll_show");
   } else {
-    subHeader.classList.remove("subHeader_hide");
-    mainheader.classList.remove("mainHeader_hide");
+    // subHeader.classList.remove("subHeader_hide");
+    // mainheader.classList.remove("mainHeader_hide");
 
-    scrollHeader.classList.remove("headerOnScroll_show");
+    // scrollHeader.classList.remove("headerOnScroll_show");
+    rightCornerHeadings.classList.remove("changeColorToBlack");
+    globeIcon.classList.remove("changeColorToBlack");
+    siteName.classList.remove("changeColorToPink");
+    siteIcon.classList.remove("changeColorToPink");
   }
 }
 window.addEventListener("scroll", hidescr);
